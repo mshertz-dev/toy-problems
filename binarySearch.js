da62a22b5fb2cf04650138f75c6706dd1d5940bf
Mike Shertz <shertzm2@gmail.com>
2020-10-12 19:29:07 -0400

    working solution to Binary Search prompt

---
+++ binarySearch.js
@@ -0,0 +1,26 @@
+//Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
+
+/*
+@param {number[]} nums
+@param {number} target
+@return {number}
+ */
+var search = function(nums, target) {
+  debugger;
+let start = 0;
+let end = nums.length - 1;
+let midpoint = Math.floor((end + start) / 2);
+
+while (end  >= start) {
+  if (nums[midpoint] === target) {
+    return midpoint;
+  } else if (nums[midpoint] > target) {
+    end = midpoint - 1;
+    midpoint = Math.floor((end + start) / 2);
+  } else if (nums[midpoint] < target) {
+    start = midpoint + 1;
+    midpoint = Math.floor((end + start) / 2);
+  }
+  }
+  return -1;
+};
