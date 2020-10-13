11aad8b3970fcc1bdb096ae2e3ae8ca03e9b705c
Mike Shertz <shertzm2@gmail.com>
2020-10-13 17:55:50 -0400

    working solution for average salary

---
+++ averageSalary.js
@@ -0,0 +1,19 @@
+// Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+// Return the average salary of employees excluding the minimum and maximum salary.
+
+// Input: salary = [4000,3000,1000,2000]
+// Output: 2500.00000
+// Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+// Average salary excluding minimum and maximum salary is (2000+3000)/2= 2500
+
+/*
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+  salary.sort((a, b) => a - b);
+  salary.shift();
+  salary.pop();
+  return salary.reduce((a, b) => a + b) / salary.length;
+};
